2dcc2d54abfea32b6bbda4235411d01fbf5a4aa8
adams2a7 <adams2a7@mail.uc.edu>
2021-01-26 16:44:56 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,33 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("M&M Color Counts");
+
+  int totalMM;
+  double blue, brown, green, orange, red, yellow;
+  totalMM = 55 * 11;
+  blue = totalMM * .24;   
+  brown = totalMM * .13;
+  green = totalMM * .16;
+  orange = totalMM * .20;
+  red = totalMM * .13;
+  yellow = totalMM * .14;
+
+  System.out.println("blue: " + blue);
+  System.out.println("brown: " + brown);
+  System.out.println("green: " + green);
+  System.out.println("orange: " + orange);
+  System.out.println("red: " + red);
+  System.out.println("yellow: " + yellow);
+
+  double Sum;
+  Sum = blue + brown + green + orange + red + yellow;
+
+  System.out.println("Sum: " + Sum);
+
+ 
+  
+
+
+    
+  }
+}
